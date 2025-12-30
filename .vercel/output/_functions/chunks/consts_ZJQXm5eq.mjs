55713a5b31f80367a544d544ccb3fbb2e14f609c
luciano azzigotti <lucianoazzigotti@gmail.com>
2025-12-30 14:02:43 -0300

    wl3

--- .vercel/output/_functions/chunks/consts_ZJQXm5eq.mjs
+++
@@ -1,4 +0,0 @@
-const SITE_TITLE = "LUCIANO AZZIGOTTI";
-const SITE_DESCRIPTION = "Luciano Azzigotti composer";
-
-export { SITE_DESCRIPTION as S, SITE_TITLE as a };
--- .vercel/output/_functions/chunks/content-modules_L2PBgyiH.mjs
+++
@@ -1,4 +0,0 @@
-const contentModules = new Map([
-["src/content/blog/using-mdx.mdx", () => import('./using-mdx_NLNR-F0Q.mjs')]]);
-
-export { contentModules as default };
